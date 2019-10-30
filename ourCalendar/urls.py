9ab8fe066b8b87c1f287d47962b89daa6ec5e0e1
FranklinEriksen <s153200@student.dtu.dk>
2019-10-30 19:58:10 -0400

    Calendar works

--- ourCalendar/urls.py
+++ ourCalendar/urls.py
@@ -7,4 +7,5 @@ app_name = 'ourCalendar'
 urlpatterns = [
     # url(r'^index/$', views.index, name='index'),
     url('test', views.CalendarView.as_view(), name='ourCalendar'), # here
+    url(r'^calendar/$', views.CalendarView.as_view(), name='ourCalendar'),  # here
 ]
