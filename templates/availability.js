cdfd3b16eea3233c33bc385b06b04bba5bd71d07
djb72498 <david.b98765@gmail.com>
2019-10-30 18:54:49 -0400

    added a few more pages

---
+++ templates/availability.js
@@ -0,0 +1,32 @@
+
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+var daySelectors = document.querySelector('form input');
+
+function createHiddenTimeInputs( day ) {
+	
+	var d = document.createElement('div');
+	
+	var startLabel = document.createElement('p');
+	startLabel.innerText = 'Start Time:';
+	
+	var startInput = document.createElements('input');
+	startInput.type = 'text';
+	
+}
+
+function updateTimeDisplay( day, b ) {
+	
+	var d = getElementById( day + '-times' );
+	var b = getElementById( day + '-box' );
+	
+	if ( b.checked )
+		d.style.display = 'block';
+	else
+		d.style.display = 'none';
+}
+
+getElementById('monday-box').addEventListener('click', updateTimeDisplay('monday'));
+
+
