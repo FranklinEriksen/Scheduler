292c5d540d514f8264e31de44ec184ae37c7154f
FranklinEriksen <s153200@student.dtu.dk>
2019-10-31 16:50:51 -0400

    Calendar works

--- accounts/views.py
+++ accounts/views.py
@@ -13,3 +13,17 @@ class SignUpEmp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('signup-employee')
     template_name = 'signup-employee.html'
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
